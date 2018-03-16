214003d1c5311586a44bc1dc5d5c397f9198d719
Canaan Seaton <cmseaton42@gmail.com>
2018-03-15 22:54:26 -0400

    Update webpack.dev.js

---
+++ config/webpack.dev.js
@@ -0,0 +1,87 @@
+const webpack = require("webpack");
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const autoprefixer = require("autoprefixer");
+
+
+module.exports = {
+    entry: {
+        bundle: "./src/index.js"
+    },
+    mode: "development",
+    output: {
+        path: path.join(__dirname, "../dist"),
+        filename: "[name].[hash:8].js"
+    },
+    module: {
+        rules: [
+            {
+                use: "babel-loader",
+                test: /\.js$/,
+                exclude: /node_modules/
+            },
+            {
+                use: "json-loader",
+                test: /\.json$/
+            },
+            {
+                test: /\.docthat$/,
+                use: [
+                    {
+                        loader: path.resolve(__dirname, "./loaders/doc-loader.js")
+                    }
+                ]
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    "style-loader",
+                    { loader: "css-loader", options: { importLoaders: 1 } },
+                    "sass-loader",
+                    {
+                        loader: "postcss-loader",
+                        options: {
+                            // Necessary for external CSS imports to work
+                            // https://github.com/facebookincubator/create-react-app/issues/2677
+                            ident: "postcss",
+                            plugins: () => [
+                                require("postcss-flexbugs-fixes"),
+                                autoprefixer({
+                                    browsers: [
+                                        ">1%",
+                                        "last 4 versions",
+                                        "Firefox ESR",
+                                        "not ie < 9" // React doesn't support IE8 anyway
+                                    ],
+                                    flexbox: "no-2009"
+                                })
+                            ]
+                        }
+                    }
+                ]
+            },
+            {
+                exclude: [/\.(js|jsx|mjs)$/, /\.html$/, /\.scss$/, /\.json$/, /\.docthat$/],
+                loader: "file-loader",
+                options: {
+                    name: "static/media/[name].[hash:8].[ext]"
+                }
+            }
+        ]
+    },
+    devServer: {
+        contentBase: "dist",
+        overlay: true,
+        hot: true
+    },
+    plugins: [
+        new webpack.NamedModulesPlugin(),
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.DefinePlugin({
+            "process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV)
+        }),
+        new HtmlWebpackPlugin({
+            template: "./src/index.html"
+        })
+    ]
+};
